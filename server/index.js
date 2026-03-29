3e3c342cfb28eca1a53e56ecb1d0352fdc2d9923
DevNasim01 <sknasimpc@gmail.com>
2026-03-29 09:45:53 +0530

    -- added route for Uptime Robot

--- server/index.js
+++ server/index.js
@@ -185,6 +185,11 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Job Board API");
 });
 
+// for Uptime rooboot to keep MO cluster alive
+app.get("/api/ping", (req, res) => {
+  res.send("Server is alive");
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
